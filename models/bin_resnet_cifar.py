fc0b19e476b373d45bc27f31a44d2728049b458d
lazyCodes7 <rishabmudliar@gmail.com>
2022-01-24 18:16:35 +0530

    Add: Support for ResNet50 models on CIFAR-10

--- models/bin_resnet_cifar.py
+++ models/bin_resnet_cifar.py
@@ -7,6 +7,9 @@ import math
 from .quantization import *
 
 
+model_urls = {
+    'resnet50': 'https://github.com/iamsh4shank/BFA_ViT/blob/main/cifar-10_models/cifar_resnet50.pt',
+}
 class DownsampleA(nn.Module):
     def __init__(self, nIn, nOut, stride):
         super(DownsampleA, self).__init__()
@@ -59,7 +62,103 @@ class ResNetBasicblock(nn.Module):
 
         return F.relu(residual + basicblock, inplace=True)
 
+class ResNet(nn.Module):
+    def __init__(self,
+                 block,
+                 layers,
+                 num_classes=1000,
+                 zero_init_residual=False):
+        super(ResNet, self).__init__()
+        self.inplanes = 64
+        self.conv1 = quan_Conv2d(3,
+                                 64,
+                                 kernel_size=7,
+                                 stride=2,
+                                 padding=3,
+                                 bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = quan_Linear(512 * block.expansion, num_classes)
 
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight,
+                                        mode='fan_out',
+                                        nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+class CifarResNet50(nn.Module):
+    def __init__(self, out_classes):
+      super(CifarResNet50, self).__init__()
+      self.arch = ResNet(Bottleneck, [3, 4, 6, 3])
+      self.fc1 = quan_Linear(1000, 500)
+      self.bn1 = nn.BatchNorm1d(500)
+      self.fc2 = quan_Linear(500, 200)
+      self.bn2 = nn.BatchNorm1d(200)
+      self.fc3 = quan_Linear(200, 100)
+      self.bn3 = nn.BatchNorm1d(100)
+      self.fc4 = quan_Linear(100, out_classes)
+      self.relu = nn.ReLU()
+
+    def forward(self, image):
+      output = self.arch(image)
+      output = self.relu(self.bn1(self.fc1(output)))
+      output = self.relu(self.bn2(self.fc2(output)))
+      output = self.relu(self.bn3(self.fc3(output)))
+      output = self.relu(self.fc4(output))
+      return output
+      
 class CifarResNet(nn.Module):
     """
   ResNet optimized for the Cifar dataset, as specified in
@@ -141,4 +240,11 @@ def resnet20_bin(num_classes=10):
     num_classes (uint): number of classes
   """
     model = CifarResNet(ResNetBasicblock, 20, num_classes)
+    return model
+
+def resnet50_bin(num_classes=10, pretrained = True):
+    model = CifarResNet50(num_classes)
+    if(pretrained):
+      model.load_state_dict(model_zoo.load_url(model_urls['resnet50'])
+      
     return model
