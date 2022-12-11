0dc83ec0f13b43072511d03165398ec9a23371c6
Rishab Mudliar <rishabmudliar@gmail.com>
2022-12-11 13:57:31 +0530

    Add: resnet50 quantized

--- models/quan_resnet_imagenet.py
+++ models/quan_resnet_imagenet.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.utils.model_zoo as model_zoo
 from .quantization import *
+import torchvision.models as models
 from transformers import ViTFeatureExtractor, ViTForImageClassification
 __all__ = [
     'ResNet', 'resnet18_quan', 'resnet34_quan', 'resnet50', 'resnet101',
@@ -182,7 +183,136 @@ class ResNet(nn.Module):
         x = self.fc(x)
 
         return x
-  
+
+
+
+# Block used in the ResNet layers
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
+        super(Block, self).__init__()
+        # Resnet 50 has 4 times the initial input channels
+        self.expansion = 4
+        # First layer
+        self.conv1 = conv1x1(in_channels, out_channels, stride=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        
+        # Second layer
+        self.conv2 = conv3x3(out_channels, out_channels, stride=stride)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        # Third layer
+        self.conv3 = conv1x1(out_channels, out_channels*self.expansion, stride=1)
+        self.bn3 = nn.BatchNorm2d(out_channels*self.expansion)
+        
+        # Activation function
+        self.relu = nn.ReLU()
+        
+        # Used to convert the shape such that it can be added properly to the output of the block
+        self.identity_downsample = identity_downsample
+    
+    # Forward propagation 
+    def forward(self, x):
+        identity = x.clone()
+        x = self.bn1(self.conv1(x))
+        x = self.relu(x)
+        x = self.bn2(self.conv2(x))
+        x = self.relu(x)        
+        x = self.bn3(self.conv3(x))
+        
+        if self.identity_downsample is not None:
+            identity = self.identity_downsample(identity)
+        
+        x += identity
+        x = self.relu(x)
+        # Returning the variable after passing through the convolutional layers
+        return x
+
+class Resnet(nn.Module):
+    def __init__(self, block, layers, image_channels, out_classes):
+        super(Resnet, self).__init__()
+        self.in_channels = 64
+        self.conv1 = quan_Conv2d(image_channels, self.in_channels, kernel_size = 7, stride = 2, padding =3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
+        # Essentially the entire ResNet architecture are in these 4 lines below
+        self.layer1 = self._make_layer(
+            block, layers[0], intermediate_channels=64, stride=1
+        )
+        self.layer2 = self._make_layer(
+            block, layers[1], intermediate_channels=128, stride=2
+        )
+        self.layer3 = self._make_layer(
+            block, layers[2], intermediate_channels=256, stride=2
+        )
+        self.layer4 = self._make_layer(
+            block, layers[3], intermediate_channels=512, stride=2
+        )
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = quan_Linear(512 * 4, out_classes)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight,
+                                        mode='fan_out',
+                                        nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+        
+        
+        
+        
+    def _make_layer(self, block, num_residual_blocks, intermediate_channels, stride):
+        identity_downsample = None
+        layers = []
+        
+        # For first layer, identity_downsample is not used since stride = 1
+        if(stride!=1 or self.in_channels != intermediate_channels*4):
+            identity_downsample = nn.Sequential(
+                # Converting into channels*4 since that happens at the end of each block in ResNet
+                conv1x1(
+                    self.in_channels,
+                    intermediate_channels*4,
+                    stride = stride                    
+                ),
+                nn.BatchNorm2d(intermediate_channels*4)
+                
+            )
+        '''This will be the first layer and we use the identity_downsample to convert the input to similiar
+        size which will then be added to the output from the block after applying the conv layers'''
+        
+        layers.append(
+            block(self.in_channels, intermediate_channels, identity_downsample, stride)
+        )
+        self.in_channels = intermediate_channels * 4
+        
+        # For example for first resnet layer: 256 will be mapped to 64 as intermediate layer,
+        # then finally back to 256. Hence no identity downsample is needed, since stride = 1,
+        # and also same amount of channels.
+        
+        for i in range(num_residual_blocks - 1):
+            layers.append(block(self.in_channels, intermediate_channels))
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc(x)
+        
+        return x
+
+
+
 class ViTClassifier(nn.Module):
     def __init__(self):
       super(ViTClassifier, self).__init__()
@@ -227,14 +357,11 @@ def resnet34_quan(pretrained=True, **kwargs):
         model.load_state_dict(model_dict)
     return model
 
-def resnet50_quan(pretrained=True, **kwargs):
-    """Constructs a ResNet-50 model.
 
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
-    if pretrained:
+
+def resnet50_quan(img_channel=3, num_classes=1000, pretrained = True):
+      model = Resnet(Block, [3, 4, 6, 3], 3, 1000)
+      if pretrained:
         pretrained_dict = model_zoo.load_url(model_urls['resnet50'])
         model_dict = model.state_dict()
         pretrained_dict = {
@@ -243,20 +370,7 @@ def resnet50_quan(pretrained=True, **kwargs):
         }
         model_dict.update(pretrained_dict)
         model.load_state_dict(model_dict)
-    return model
-
-
-def resnet50(pretrained=False, **kwargs):
-    """Constructs a ResNet-50 model.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
-    return model
-
+      return model
 
 def resnet101(pretrained=False, **kwargs):
     """Constructs a ResNet-101 model.
